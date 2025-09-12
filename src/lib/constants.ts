a6fae363caeeb3392401ce81cc3971eb051436fe
franz <franzns11@gmail.com>
2025-09-12 14:08:14 +0200

    add 24 again

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const API_URL = 'https://backend-v3.beets-ftm-node.com/graphql';
 
-export const ALLOWED_VALIDATORS = ['13', '14', '15', '16', '17', '18', '23', '29', '30', '39', '44', '45'];
+export const ALLOWED_VALIDATORS = ['13', '14', '15', '16', '17', '18', '23', '24', '29', '30', '39', '44', '45'];
 
 export const stSAddress = '0xE5DA20F15420aD15DE0fa650600aFc998bbE3955';
 export const SFC_ADDRESS = '0xFC00FACE00000000000000000000000000000000';
